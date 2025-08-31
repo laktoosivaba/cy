3c82b270eeb0a5f5879ca90755865b8aab01087e
Andrey Shinkarev <andrey@bobw.co>
2025-08-31 20:08:27 +0300

    build commands for andrey

---
+++ wasm_build.sh
@@ -0,0 +1,5 @@
+mkdir build
+cd build
+emmake cmake ..
+emmake make clean && emmake make
+emcc cy_wasm/libcy_wasm.a cy/libcy.a -o /Users/andrey/Projects/rust/cy_rust/lib/libcy_wasm.wasm -sSTANDALONE_WASM --no-entry -sEXPORTED_FUNCTIONS=_cy_new_wasm,_malloc
